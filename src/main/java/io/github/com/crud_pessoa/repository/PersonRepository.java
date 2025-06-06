04792d9df4ec4e789e2c438912beaee90e751a99
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-06 07:46:45 -0300

    feat: Create a new method in the repository to search for the user's CPF in the database before persisting in order to avoid duplicate CPFs.

--- src/main/java/io/github/com/crud_pessoa/repository/PersonRepository.java
+++ src/main/java/io/github/com/crud_pessoa/repository/PersonRepository.java
@@ -4,4 +4,5 @@ import io.github.com.crud_pessoa.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    boolean existsByCPF(String cpf);
 }
